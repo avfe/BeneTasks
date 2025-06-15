8f72780b04e2e7fef303d77ef7c15f2e629fb07b
arseny <5Pnt;v~2BF@XePB>
2025-06-15 22:42:41 +0300

    Проект разбит на директории, внесены изменения связанные с Docker

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ DB_PASS = os.getenv("POSTGRES_PASSWORD")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
